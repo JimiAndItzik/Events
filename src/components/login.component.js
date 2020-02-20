c757b0b1e3ba1fcd1d38ef5656a9e236cc9d5bab
itzikooper <itzikooper@gmail.com>
2020-02-20 21:12:48 +0200

    Added "Guide.txt" file and change site description

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import './css/login.css'
+import './css/login.css';
 
 export default class Login extends Component {
 
